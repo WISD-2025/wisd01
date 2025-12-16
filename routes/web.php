a5a57f4d8972fcab398fb6874cd88a5240cf4c20
Huang <3b232009@gm.student.ncut.edu.tw>
2025-12-17 05:32:55 +0800

    新增 dd.d2 和 route

--- routes/web.php
+++ routes/web.php
@@ -35,6 +35,10 @@ Route::middleware(['auth'])->group(function () {
     return view('dd.d1');
     });
 
+    Route::get('/dd/d2', function () {
+    return view('dd.d2');
+    });
+
     Volt::route('settings/profile', 'settings.profile')->name('profile.edit');
     Volt::route('settings/password', 'settings.password')->name('user-password.edit');
     Volt::route('settings/appearance', 'settings.appearance')->name('appearance.edit');
